feda0bd749910c65a98765211d4cd67caa50f2e3
Prescott Murphy <prurph@gmail.com>
2021-04-16 17:52:16 -0400

    Use css styling instead of Prism for inline code

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,21 @@
 import { Container } from "theme-ui";
 
 export default function Layout({ children }) {
-  return <Container p={4}>{children}</Container>;
+  return (
+    <Container p={4}>
+      {children}
+      <style jsx global>{`
+        :not(pre) > code {
+          background-color: #2e3440;
+          color: var(--theme-ui-colors-background);
+          font-family: "Fira Code", Monaco, "Ubuntu Mono", monospace;
+          padding: 0.1em;
+          margin: 0 0.1em;
+          border-radius: 0.2em;
+          white-space: normal;
+          font-size: 90%;
+        }
+      `}</style>
+    </Container>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -46,21 +46,6 @@ export default function Home({ sortedPosts }) {
           list-style-type: none;
         }
       `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
